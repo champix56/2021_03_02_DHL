2a284f2187f0ef0c3b670ba41248097b5c000830
alex <desorbaix@free.fr>
2021-03-02 20:07:50 +0100

    mie en place d'un fuchier js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// chargement dans une variable de la balise <div id="jsLoaded">...</div>
+var jsloaded=document.querySelector("#jsLoaded");
+
+//moddif du style css de la balise
+jsloaded.style.backgroundColor="green";
+
+//moddif du contenu textuelle de la balise
+jsloaded.innerText="Le js est chargé";
+
+console.log("Le fichier index.js a fini d'etre executer");
