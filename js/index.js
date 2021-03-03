e294222d43d8fa21ac4d740cd0d0f253c5472b41
alex <desorbaix@free.fr>
2021-03-03 14:48:26 +0100

    declaration et usage de function

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-// chargement dans une variable de la balise <div id="jsLoaded">...</div>
-var jsloaded=document.querySelector("#jsLoaded");
+function jsIsLoaded() {
+    // chargement dans une variable de la balise <div id="jsLoaded">...</div>
+    var jsloaded = document.querySelector("#jsLoaded");
 
-//moddif du style css de la balise
-jsloaded.style.backgroundColor="green";
+    //moddif du style css de la balise
+    jsloaded.style.backgroundColor = "green";
 
-//moddif du contenu textuelle de la balise
-jsloaded.innerText="Le js est chargé";
+    //moddif du contenu textuelle de la balise
+    jsloaded.innerText = "Le js est chargé";
 
-console.log("Le fichier index.js a fini d'etre executer");
+    console.log("Le fichier index.js a fini d'etre executer");
 
-console.log('essaie');
+    console.log('essaie');
+
+}
+jsIsLoaded();
