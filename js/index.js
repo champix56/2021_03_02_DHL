90fa40362058308af16c403bc23e8fe68b398b5f
alex <desorbaix@free.fr>
2021-03-03 15:22:26 +0100

    recuperation des champs de formulaire

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function jsIsLoaded() {
     var jsloaded = document.querySelector("#jsLoaded");
 
     //moddif du style css de la balise
-    jsloaded.style.backgroundColor = "green";
+    jsloaded.style.backgroundColor = "skyblue";
 
     //moddif du contenu textuelle de la balise
     jsloaded.innerText = "Le js est chargé";
@@ -13,4 +13,16 @@ function jsIsLoaded() {
     console.log('essaie');
 
 }
+
 jsIsLoaded();
+
+function logFormulaire() {
+    var formulaire = document.forms['mon-form'];
+    console.log('titre :', formulaire['form-titre'].value);
+    console.log('auteurId :', formulaire['form-auteur'].value);
+    console.log('date :', formulaire['form-date'].value);
+    console.log('heure :', formulaire['form-hour'].value);
+    console.log('adresse :', formulaire['form-adresse'].value);
+    console.log('email', document.querySelector('form #form-email').value);
+    console.log('description', formulaire['form-description'].value);
+}
