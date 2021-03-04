4eb8ea235d1f9551cce48d760abbf0c05c0fec8f
alex <desorbaix@free.fr>
2021-03-04 10:15:16 +0100

    mise en place d'une verif de remplissage

--- js/index.js
+++ js/index.js
@@ -16,18 +16,29 @@ function jsIsLoaded() {
 
 jsIsLoaded();
 
+/**
+ * verifie le remplissage du formulaire
+ * @returns Boolean faux des qu'un champs est vide
+ */
+function isFormFullFill() {
+    for (var input of document.forms['mon-form']) {
+        if(input.value==='')return false;
+    }
+    return true;
+}
+
 function getFormulaire() {
     //access a la balise form par document.forms
     var formulaire = document.forms['mon-form'];
     //constitution d'un objet avec les champs issue du forms
-    var unPostIt={
-        titre:formulaire['form-titre'].value,
-        auteurId:formulaire['form-auteur'].value,
-        date:formulaire['form-date'].value,
-        heure:formulaire['form-hour'].value,
-        adresse:formulaire['form-adresse'].value,
-        mail:formulaire['form-email'].value,
-        description:formulaire['form-description'].value
+    var unPostIt = {
+        titre: formulaire['form-titre'].value,
+        auteurId: formulaire['form-auteur'].value,
+        date: formulaire['form-date'].value,
+        heure: formulaire['form-hour'].value,
+        adresse: formulaire['form-adresse'].value,
+        mail: formulaire['form-email'].value,
+        description: formulaire['form-description'].value
     }
     console.log(unPostIt);
     return unPostIt;
@@ -36,25 +47,31 @@ function getFormulaire() {
  * Clone d'un postit modele et remplissage des valeurs puis ajout à la liste
  * @param {Postit} postitValues objet comprennant les valeurs d'un postit a afficher
  */
-function makePostIt(postitValues){
+function makePostIt(postitValues) {
     //recuperation du postit model pour la creation des autres postit a remplir
     //clone permet d'obtenir un double non lié a l'element d'origine
-    var postitNode=document.querySelector('.post-it').cloneNode(true);
+    var postitNode = document.querySelector('.post-it').cloneNode(true);
     //composition d'un post it rempli avec les valeurs recus en argument d'entree de fonction
-    postitNode.querySelector('.post-it-titre').innerText=postitValues.titre;
-    postitNode.querySelector('.post-it-adresse').innerText=postitValues.adresse;
-    postitNode.querySelector('.post-it-mail').innerText=postitValues.mail;
-    postitNode.querySelector('.post-it-date').innerText='Le '+postitValues.date+' a '+postitValues.heure;
-    postitNode.querySelector('.post-it-description').innerText=postitValues.description;
-    postitNode.querySelector('.post-it-auteur').innerText=postitValues.auteurId;
+    postitNode.querySelector('.post-it-titre').innerText = postitValues.titre;
+    postitNode.querySelector('.post-it-adresse').innerText = postitValues.adresse;
+    postitNode.querySelector('.post-it-mail').innerText = postitValues.mail;
+    postitNode.querySelector('.post-it-date').innerText = 'Le ' + postitValues.date + ' a ' + postitValues.heure;
+    postitNode.querySelector('.post-it-description').innerText = postitValues.description;
+    postitNode.querySelector('.post-it-auteur').innerText = postitValues.auteurId;
 
     //ajout à la fin de la liste du postit cloné et rempli 
     document.querySelector('#post-it-liste').append(postitNode);
 }
-function onformsubmit(evt){
+function onformsubmit(evt) {
+    //arret de l'execution par defaut de la soumission (rechargement de page)
     evt.preventDefault();
+    //si le formulaire est pas rempli je sors de cette fonction
+    if(!isFormFullFill())return;
+    //recuperation des valeur dans le formulaire
     var postitValues = getFormulaire();
+    //creation du postit rempli
     makePostIt(postitValues);
+    //reset du contenu du form
     evt.target.reset();
 }
-document.forms['mon-form'].addEventListener('submit',onformsubmit);
+document.forms['mon-form'].addEventListener('submit', onformsubmit);
