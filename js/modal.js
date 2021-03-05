89564c5b93721bcb61c60575bde0c0f0d968f3f9
alex <desorbaix@free.fr>
2021-03-05 16:05:43 +0100

    mise en place d'un system de modal a une ou deux reponse

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+function showModal(message, actionOk, actionCancel) {
+    var modalInner = '<div id="modal" style="background-color: rgba(128, 128, 128, 0.89) ;top:0;bottom:0;left:0;right:0;position: absolute;padding:20vw 15vh;text-align: center;">\
+            <div id="modal-content" style="border-radius:7px;background-color: rgba(255, 255, 255, 1);height:25vh;width:400px;padding-top:40px;margin-left:calc(50vw - 400px)">\
+                <div id="modal-text" style="text-align: center; height:80%">text de la modal</div>\
+                <div id="modal-button" style="text-align: center;"><button class="btn btn-success">btn</button></div>\
+            </div>\
+        </div>';
+}
